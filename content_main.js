4c60c6ab406afd29c471bbbea20bcf5148bd66de
Manvendra Singh <manvendra0310@gmail.com>
2019-10-09 00:08:02 +0530

    content script: handler for observing page visibility changes

--- content_main.js
+++ content_main.js
@@ -1,5 +1,7 @@
 // 'use strict';
 
+// import { Timer } from './timer';
+
 // Change to 'true' to enable logging. 
 // NOTE: this is manual because 
 // limited chrome API is available for content script
@@ -11,10 +13,34 @@ if (!DEBUG_MODE_ON) {
     console.log = function () { };
 }
 
+function toggleTimer() {
+    // console.log('Document hidden status: ' + document.hidden)
+    if (document.visibilityState !== 'visible') {
+        // stop timer
+        console.log('Stopping timer')
+    } else {
+        // continue timer
+        console.log('Resuming timer')
+    }
+}
+
+// Handle all state mangement related to 
+// visiblity change of the given page
+// A page is 'hidden' whenever the browser window
+// is minimised or the current tab is not in focus.
+// Refer: https://developer.mozilla.org/en-US/docs/Web/API/Page_Visibility_API
+function handleVisibilityChange() {
+    toggleTimer()
+}
 
 // Main function which will be called from 'content_main.js'
-// this separation is done to enable use of ES6 'import' feature.  
-// Refer: https://medium.com/@otiai10/how-to-use-es6-import-with-chrome-extension-bd5217b9c978
+// This separation is done to enable use of ES6 features such as 'import'.  
+// Refer: https://medium.com/@otiai10/how-to-use-es6-import-wixth-chrome-extension-bd5217b9c978
 export function main() {
     console.log('This is the content script and should be visible in the console..')
+    
+    // let t = Timer();
+    // console.log('Successfully created new timer..')
+
+    document.addEventListener('visibilitychange', handleVisibilityChange, false)
 }
